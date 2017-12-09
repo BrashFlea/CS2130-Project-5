194714de536ed9350ef1b2c7a89459ccac2f5d0f
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-12-08 23:30:13 -0700

    Completed Tests for L1 in Part B

--- src/cs2130p5/TestFSM.java
+++ src/cs2130p5/TestFSM.java
@@ -1,29 +1,48 @@
+// Project #5 test program
 package cs2130p5;
 
 public class TestFSM {
   
   public static void main(String[] args)
   {
-    // Language: strings that contain both a and b,
-    // and the first a is before the first b.
-    String A   = "abc";
-    int[][] ST = {{1,3,0},
-                  {1,2,1},
-                  {2,2,2},
-                  {3,3,3}};
-    int[] AS   = {0,0,1,0};
 
-    String inString;
-    boolean accept1 = false;
-    FSM FSM1 = new FSM(A, ST, AS);
-    if(args.length >= 1)
-    {
-      // Input string is command line parameter
-      inString = args[0];
-      accept1 = FSM1.validString(inString);
-      System.out.println("\n String:  " + inString 
-        + "    Accept?  " + accept1);
-    }
+    // Language: strings that contain an odd number of a's and exactly one b. 
+    String A1 = "ab";
+    int[][] ST1 =  {{3,2},
+                    {2,4},
+                    {1,4},
+                    {0,1},
+                    {4,4}};
+
+    int[] AS1 = {0,1,0,0,0};
+
+    FSM L1 = new FSM (A1, ST1, AS1);
+
+    String inString1a = "aabaaaa";
+    String inString1b = "aaabaaaa";
+    String inString1c = "aabaaaab";
+    String inString1d = "baaaaaa";
+    String inString1e = "aaaaabaa";
+
+    boolean accept1a = L1.validString(inString1a);
+    boolean accept1b = L1.validString(inString1b);
+    boolean accept1c = L1.validString(inString1c);
+    boolean accept1d = L1.validString(inString1d);
+    boolean accept1e = L1.validString(inString1e);
+
+    System.out.println("L1:");
+    System.out.println("String: " + inString1a + "   Accept?  " + accept1a);
+    System.out.println("String: " + inString1b + "   Accept?  " + accept1b);
+    System.out.println("String: " + inString1c + "   Accept?  " + accept1c);
+    System.out.println("String: " + inString1d + "   Accept?  " + accept1d);
+    System.out.println("String: " + inString1e + "   Accept?  " + accept1e);
+    addLine();
+
+
   } // end main
+
+  public static void addLine() {
+    System.out.println("--------------------------------");
+  }
 
 } // end class
